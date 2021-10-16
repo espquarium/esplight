2b9a7c6b31acb237626c069a22b1ed3067952f8d
Gustavo <gustavo@obex.pw>
2021-10-16 13:28:39 -0300

    added logic to change light with ntp

--- esplight/include/light_helper.h
+++ esplight/include/light_helper.h
@@ -5,35 +5,55 @@
 
 int PINS[MAX_CHANNELS] = {27, 26, 25, 33};
 
-class LightHelper {
-   public:
+class LightHelper
+{
+public:
     bool forceLight;
 
-    LightHelper() {
+    LightHelper()
+    {
         this->forceLight = true;
     }
 
     void
-    allChannels(int brightness) {
-        for (int ch : PINS) {
+    allChannels(int brightness)
+    {
+        for (int ch : PINS)
+        {
             analogWrite(ch, brightness, MAX_BRIGHTNESS);
         }
     }
 
     void
-    perChannel(int channels[MAX_CHANNELS]) {
-        for (int ch = 0; ch < MAX_CHANNELS; ch++) {
+    perChannel(int channels[MAX_CHANNELS])
+    {
+        for (int ch = 0; ch < MAX_CHANNELS; ch++)
+        {
             analogWrite(PINS[ch], ch, MAX_BRIGHTNESS);
         }
     }
 
-    void loop() {
-        if (this->forceLight) {
+    void loop(Date timeNow, int timesSaved, LighTime lightTimes[48])
+    {
+        if (this->forceLight)
+        {
             allChannels(MAX_BRIGHTNESS);
+        }
+        else
+        {
+            for (int i = timesSaved; i != 0; i--)
+            {
+                LightTime lightSaved = lightTimes[i];
+                if (timeNow.h >= lightSaved.h && timeNow.m >= lightSaved.m)
+                {
+                    return this->perChannel(lightSaved.c);
+                }
+            }
         }
     }
 
-    void setForceLight(bool force) {
+    void setForceLight(bool force)
+    {
         this->forceLight = force;
     }
 };
--- esplight/src/main.cpp
+++ esplight/src/main.cpp
@@ -31,15 +31,18 @@ WebServer server(80);
 LightHelper light = LightHelper();
 NTPHelper ntpHelper = NTPHelper();
 
-void saveConfigCallback() {
+void saveConfigCallback()
+{
     Serial.println("Configuração salva");
 }
 
-void connectToWifi() {
+void connectToWifi()
+{
     String portalName = "ESPLIGHT-WIFI";
     printToTft(portalName);
     printToTft("Acesse seu telefone e conecte para configurar", false, 10);
-    if (!wifiManager.autoConnect(portalName.c_str())) {
+    if (!wifiManager.autoConnect(portalName.c_str()))
+    {
         printToTft("Falha ao conectar");
         espDelay(3000);
         //reset and try again, or maybe put it to deep sleep
@@ -52,9 +55,11 @@ void connectToWifi() {
     espDelay(3000);
 }
 
-void button_init() {
+void button_init()
+{
     btn1.setLongClickHandler(
-        [](Button2& b) {
+        [](Button2 &b)
+        {
             runServer = false;
             int r = digitalRead(TFT_BL);
             tft.fillScreen(TFT_BLACK);
@@ -76,47 +81,57 @@ void button_init() {
         });
 
     btn1.setClickHandler(
-        [](Button2& b) {
+        [](Button2 &b)
+        {
             runServer = true;
             printToTft("Conectado");
             printToTft("IP: " + WiFi.localIP().toString(), false, 10);
         });
 }
 
-void button_loop() {
+void button_loop()
+{
     btn1.loop();
     btn2.loop();
 }
 
-void setCrossOrigin() {
+void setCrossOrigin()
+{
     server.sendHeader(F("Access-Control-Allow-Origin"), F("*"));
     server.sendHeader(F("Access-Control-Max-Age"), F("600"));
     server.sendHeader(F("Access-Control-Allow-Methods"), F("PUT,POST,GET,OPTIONS"));
     server.sendHeader(F("Access-Control-Allow-Headers"), F("*"));
 };
 
-void handleNotFound() {
-    if (server.method() == HTTP_OPTIONS) {
+void handleNotFound()
+{
+    if (server.method() == HTTP_OPTIONS)
+    {
         setCrossOrigin();
         server.send(204);
-    } else {
+    }
+    else
+    {
         server.send(404, "text/plain", "");
     }
 }
 
-void sendCrossOriginHeader() {
+void sendCrossOriginHeader()
+{
     Serial.println(F("sendCORSHeader"));
     setCrossOrigin();
     server.send(204);
 }
 
-void handleToggle() {
+void handleToggle()
+{
     light.setForceLight(!light.forceLight);
     setCrossOrigin();
     server.send(200, "text/plain", "toggled light");
 }
 
-void handleVerify() {
+void handleVerify()
+{
     // String resText = "light is ";
     // resText += light.forceLight ? "on" : "off";
     // printToTft(resText, false, 40);
@@ -127,18 +142,21 @@ void handleVerify() {
     server.send(200, "application/json", resText);
 }
 
-void handleLightTimes() {
+void handleLightTimes()
+{
     setCrossOrigin();
     server.send(200, "application/json", lightStorage.getTimesAsJson());
 }
 
-void handleUpdateLightTimes() {
+void handleUpdateLightTimes()
+{
     lightStorage.save(server.arg(0));
     setCrossOrigin();
     server.send(200, "application/json", lightStorage.getTimesAsJson());
 }
 
-void setup() {
+void setup()
+{
     Serial.begin(9600);
     Serial.println("Start");
     lightStorage.setup();
@@ -156,7 +174,8 @@ void setup() {
     connectToWifi();
 
     server.on("/", HTTP_GET,
-              []() { server.send(200, "text/plain", "Welcome to reeflight :)"); });
+              []()
+              { server.send(200, "text/plain", "Welcome to reeflight :)"); });
 
     server.on("/toggle", HTTP_GET,
               handleToggle);
@@ -177,10 +196,16 @@ void setup() {
     server.begin();
 }
 
-void loop() {
+void loop()
+{
     button_loop();
-    light.loop();
-    if (runServer) {
+
+    Date timeNow = ntpHelper.getTime();
+
+    light.loop(timeNow, lightStorage.timesSaved, lightStorage.lightTimes);
+
+    if (runServer)
+    {
         server.handleClient();
     }
 }
